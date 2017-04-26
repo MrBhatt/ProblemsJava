e0bc87298676cbf1d7735ba9ff8cd2a543bb283d
Anupam Bhatt <anupam.bhatt@pb.com>
2017-04-26 18:31:21 +0530

    Add initial code

---
+++ TowerOfHanoi.java
@@ -0,0 +1,12 @@
+public class TowerOfHanoi {
+  public static void main(String[] args) {
+    // PROBLEM: Given a tower (source) of discs, arranged in a decreasing order of disc size, move all the discs to
+    // another tower (target) in the same descending order of size. Another free/spare tower is made available to
+    // facilitate the movement
+    // RULES:
+    //
+
+    int numberOfDiscs = 3;
+
+  }
+}
